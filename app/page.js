dce77ca7b5bd98bf6b120b202b8a7dd60b0d70e1
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-10-09 15:45:49 -0400

    dz

--- app/page.js
+++ app/page.js
@@ -62,7 +62,7 @@ export default function Home() {
           type="text"
           className="flex-grow"
           name="prompt"
-          placeholder="Enter a prompt to display an image"
+          placeholder="Enter a promzpt to display an image"
           ref={promptInputRef}
         />
         <button className="button" type="submit">
